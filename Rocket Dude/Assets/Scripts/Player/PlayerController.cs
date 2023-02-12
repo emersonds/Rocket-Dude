33d639149c73a388efe15ee75be364351de9af51
Dylan Emerson <emersonds2868@gmail.com>
2023-02-11 23:04:32 -0500

    Final (Prototype) Movement/Jump Tweaking
    - Added IsGrounded function to check if the player is grounded. This is used for jumping to prevent infinite jumps and falling to prevent infinite bouncing (NOTE: After setting player bounciness to 0, this may be unnecessary).
    - Added JumpSaver coroutine that allows the player to jump more consistently when trying to jump while falling. How it works now is by letting the player jump as soon as they are grounded if they attempt to jump while not grounded within 0.1 seconds. This gives the player enough time to consistently and smoothly jump without being too forgiving.
    - Added stats to PlayerStats.cs for fall behavior.
    - Tweaked various physics values to make movement and jumping feel good.
    - Removed Ground_Basic physics material because it didn't seem to impact movement.

--- Rocket Dude/Assets/Scripts/Player/PlayerController.cs
+++ Rocket Dude/Assets/Scripts/Player/PlayerController.cs
@@ -6,8 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     #region Fields
 
-    private bool canJump = true;    // If the player can jump
-    private bool grounded = true;   // If the player is on ground
+    //private bool canJump = true;    // If the player can jump
+
+    [SerializeField, Tooltip("Extra distance from bottom of collider that is checked for ground/jumps.")]
+    private float groundedBuffer = 0.5f;
+
+    [SerializeField, Tooltip("How long to wait before jumping or not when the player jumps as they land.")]
+    private float jumpSaverTime = 0.25f;
 
     private float hInput = 0f;      // Where the player is moving
 
@@ -17,17 +22,21 @@ public class PlayerController : MonoBehaviour
 
     // These are components attached to the player object
 
+    private CapsuleCollider2D capsuleCollider;
     private PlayerStats stats;
     private Rigidbody2D rb2d;
-    private SpriteRenderer spriteRenderer;
 
     #endregion
+
+    [SerializeField, Tooltip("Ground layer for grounded checking and jumping/falling.")]
+    private LayerMask isGround;
 
     // Start is called before the first frame update
     void Start()
     {
         stats = GetComponent<PlayerStats>();
         rb2d = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
@@ -36,6 +45,7 @@ public class PlayerController : MonoBehaviour
         hInput = Input.GetAxisRaw("Horizontal") * stats.MoveSpeed;
 
         Jump();
+        Fall();
     }
 
     private void FixedUpdate()
@@ -45,25 +55,79 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        rb2d.AddForce(new Vector2(hInput, 0f), ForceMode2D.Impulse);
+        rb2d.AddForce(new Vector2(hInput, 0f) * Time.deltaTime, ForceMode2D.Impulse);
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && canJump && grounded)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             Debug.Log("Jump!");
 
             rb2d.AddForce(new Vector2(0f, 1f) * stats.JumpForce, ForceMode2D.Impulse);
-
-            StartCoroutine(JumpCooldown());
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !IsGrounded())
+        {
+            StartCoroutine(JumpSaver(jumpSaverTime));
         }
     }
 
-    private IEnumerator JumpCooldown()
+    private void Fall()
     {
-        canJump = false;
-        yield return new WaitForSeconds(0.7f);
-        canJump = true;
+        if (rb2d.velocity.y < 0)
+        {
+            Debug.Log("Fall!");
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (stats.FallMultiplier - 1) * Time.deltaTime;
+
+            if (IsGrounded())
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit =
+            Physics2D.BoxCast(capsuleCollider.bounds.center,
+                              capsuleCollider.bounds.size - new Vector3(0.1f, 0f, 0f),
+                              0f, Vector2.down, groundedBuffer, isGround);
+
+        // This was used for visualizing the box cast
+        /*
+        Color rayColor;
+        if (hit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(capsuleCollider.bounds.center + new Vector3(capsuleCollider.bounds.extents.x, 0), Vector2.down * (capsuleCollider.bounds.extents.y + groundedBuffer), rayColor);
+        Debug.DrawRay(capsuleCollider.bounds.center - new Vector3(capsuleCollider.bounds.extents.x, 0), Vector2.down * (capsuleCollider.bounds.extents.y + groundedBuffer), rayColor);
+        Debug.Log(hit.collider);
+        */
+
+        return hit.collider != null;
+    }
+
+    private IEnumerator JumpSaver(float timeToWait)
+    {
+        while (timeToWait > 0)
+        {
+            if (IsGrounded())
+            {
+                Debug.Log("Late jump!");
+                rb2d.AddForce(new Vector2(0f, 1f) * stats.JumpForce, ForceMode2D.Impulse);
+                yield break;
+            }
+            else
+            {
+                Debug.Log("No ground!");
+                yield return new WaitForSeconds(0.02f); // Wait approximately one frame
+                timeToWait -= 0.02f;
+            }
+        }
     }
 }
--- Rocket Dude/Assets/Scripts/Player/PlayerStats.cs
+++ Rocket Dude/Assets/Scripts/Player/PlayerStats.cs
@@ -12,6 +12,12 @@ public class PlayerStats : MonoBehaviour
     [SerializeField, Tooltip("How much force the player uses to jump.")]
     private float jumpForce = 0.3f;
 
+    [SerializeField, Tooltip("Increase in gravity for better falling.")]
+    private float fallMultiplier = 2.5f;
+
+    [SerializeField, Tooltip("Increase in gravity for low jumps.")]
+    private float lowJumpMultiplier = 2f;
+
     #endregion
 
     #region Properties
@@ -19,6 +25,10 @@ public class PlayerStats : MonoBehaviour
     public float MoveSpeed { get { return moveSpeed; } }
 
     public float JumpForce { get { return jumpForce; } }
+
+    public float FallMultiplier { get { return fallMultiplier; } }
+
+    public float LowJumpMultiplier { get { return lowJumpMultiplier; } }
 
     #endregion
 }
