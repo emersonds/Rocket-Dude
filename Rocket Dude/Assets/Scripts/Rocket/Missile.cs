31b90da914211e7a60b557e2128061097ef0f735
Dylan Emerson <emersonds2868@gmail.com>
2023-02-12 20:31:27 -0500

    Implemented Physics-Based Explosions
    - Implemented the Explode method to have a stronger force the closer the affected object is to the center of the explosion.
    - BUG: Unity doesn't seem to be calculating my explosive force correctly, and I'm not sure how to fix this. Checking my math outside of Unity produces the correct answer, but the same math in Unity produces wildly inaccurate results. In most cases, Unity produces an answer around 5 or 6, but the correct answer according to a calculator could be anywhere from 500 to 1500. Not sure why this is.

--- Rocket Dude/Assets/Scripts/Rocket/Missile.cs
+++ Rocket Dude/Assets/Scripts/Rocket/Missile.cs
@@ -10,10 +10,11 @@ public class Missile : MonoBehaviour
     [SerializeField]
     private LayerMask ammoLayer;
 
-    private void Start()
-    {
+    private Collider2D[] inExplosionRadius = null;
 
-    }
+    [SerializeField]
+    private float explosiveForceMultiplier = 1000f;
+    private float explosionRadius = 5f;
 
     /// <summary>
     /// Runs when the missile collides with something
@@ -29,7 +30,34 @@ public class Missile : MonoBehaviour
     /// </summary>
     private void Explode()
     {
-        Debug.Log("Explode!");
+        inExplosionRadius = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+
+        foreach (Collider2D coll in inExplosionRadius)
+        {
+            Rigidbody2D collRb2d = coll.GetComponent<Rigidbody2D>();
+            if (collRb2d != null)
+            {
+                // Get distance from center of explosion to object
+                Vector2 distance = coll.transform.position - transform.position;
+
+                // Adjust explosive force depending on distance.
+                // Check greater than 0 to prevent NaN error (divide by 0)
+                if (distance.magnitude > 0)
+                {
+                    float explosiveForce = explosiveForceMultiplier / distance.magnitude;
+                    collRb2d.AddForce(distance.normalized * explosiveForce, ForceMode2D.Impulse);
+
+                    Debug.Log($"Obj: {coll.name}\nMagn: {distance.magnitude}\nexplforce: {explosiveForce}\nAddForceForce: {distance.normalized * explosiveForce}");
+                }
+            }
+        }
+
+        // Destroy object after exploding
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
